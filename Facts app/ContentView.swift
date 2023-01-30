9c0cee0ab98ce791472518b3f3614b9120a8f68a
Tyson Walker II <tywalk10@gmail.com>
2023-01-30 15:51:28 -0500

    Initial Commit

---
+++ Facts app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Facts app
+//
+//  Created by Tyson Laroyce Walker II on 1/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facts app/Facts_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Facts_appApp.swift
+//  Facts app
+//
+//  Created by Tyson Laroyce Walker II on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Facts_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
