9ed6a12fce3bf042e745e403db4a63671c2cd50f
code <ajacks75@emich.edu>
2023-01-31 14:43:26 -0500

    tabs

--- Facts app/ContentView.swift
+++ Facts app/ContentView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TabView{
+                
+                Group{
+                    Text("j")
+                        .tabItem{
+                            Image(systemName: "person.fill")
+                        }
+                    Text ("k")
+                        .tabItem{
+                            Image(systemName: "menucard.fill")
+                        }
+                    Text ("L")
+                        .tabItem{Image(systemName:"info.bubble.fill")}
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
